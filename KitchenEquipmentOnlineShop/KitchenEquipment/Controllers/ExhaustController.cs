e4a9e90979ac56988398fb20e870c47cd6302060
Alexander Bedenko <alexander.bedenko@sorted.com>
2019-03-27 11:25:15 +0300

    Added ajax loading for sidebar

---
+++ KitchenEquipmentOnlineShop/KitchenEquipment/Controllers/ExhaustController.cs
@@ -0,0 +1,20 @@
+﻿using KitchenEquipmentOnlineShop.BusinessLogic.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KitchenEquipment.Controllers
+{
+    public class ExhaustController : Controller
+    {
+        private IExhaustHoodService _exhaustHoodService;
+
+        public ExhaustController(IExhaustHoodService exhaustHoodService)
+        {
+            _exhaustHoodService = exhaustHoodService;
+        }
+
+        public IActionResult Index()
+        {
+            return PartialView("_Index");
+        }
+    }
+}
