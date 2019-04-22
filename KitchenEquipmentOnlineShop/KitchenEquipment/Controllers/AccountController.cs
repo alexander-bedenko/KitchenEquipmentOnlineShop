6449bf99e33eed1ff37ca360fb1bd611a3aa0834
Alexander Bedenko <alexander.bedenko@sorted.com>
2019-04-22 10:38:37 +0300

    Refactored login and register forms

--- KitchenEquipmentOnlineShop/KitchenEquipment/Controllers/AccountController.cs
+++ KitchenEquipmentOnlineShop/KitchenEquipment/Controllers/AccountController.cs
@@ -86,8 +86,7 @@ namespace KitchenEquipment.Controllers
         public async Task<IActionResult> Logout()
         {
             await HttpContext.SignOutAsync();
-            //return RedirectToAction("Index", "Home");
-            return Ok();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
