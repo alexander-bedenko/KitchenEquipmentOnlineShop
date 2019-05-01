eea4ee5e6d941e1b108c0da2a7f8d86edaeaaa29
alexander-bedenko <zylon1982@gmail.com>
2019-05-01 10:11:17 +0300

    Added company controller

---
+++ KitchenEquipmentOnlineShop/KitchenEquipment/Controllers/CompanyController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using KitchenEquipment.Models;
+using KitchenEquipmentOnlineShop.BusinessLogic.Dtos;
+using KitchenEquipmentOnlineShop.BusinessLogic.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
+
+namespace KitchenEquipment.Controllers
+{
+    public class CompanyController : Controller
+    {
+        private ICompanyService _companyService;
+        private int pageSize = 8;
+
+        public CompanyController(ICompanyService companyService)
+        {
+            _companyService = companyService;
+        }
+
+        public IActionResult Index(int? page)
+        {
+            int pageNumber = (page ?? 1);
+            var companies = Mapper.Map<IEnumerable<CompanyViewModel>>(_companyService.GetAll());
+            return PartialView("Index", companies.ToPagedList(pageNumber, pageSize));
+        }
+
+        [Authorize]
+        public IActionResult Create(string url)
+        {
+            ViewBag.Url = url;
+            return PartialView("_Create");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CompanyViewModel company)
+        {
+            if (ModelState.IsValid)
+            {
+                await _companyService.Create(Mapper.Map<CompanyDto>(company));
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
